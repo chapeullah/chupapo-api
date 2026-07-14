d9f3e2bf83ff6db15a6047afa5884ebe2c9f831d
chapeullah <chapeullah@gmail.com>
2026-07-14 13:56:56 +0300

    Initial commit

---
+++ src/main/java/org/chapeullah/chupapoapi/ChupapoApiApplication.java
@@ -0,0 +1,13 @@
+package org.chapeullah.chupapoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChupapoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChupapoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chapeullah/chupapoapi/ChupapoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chapeullah.chupapoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChupapoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
